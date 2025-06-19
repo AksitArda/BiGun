c76302b95ae58c5e4b994a7e4bfad172b2e12411
AksitArda <aksithuseyinarda@gmail.com>
2025-06-20 00:37:09 +0300

    cors regex

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:56982'],
+    origin: ['*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
